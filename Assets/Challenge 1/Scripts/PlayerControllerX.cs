6650e18a155714adea05237336e523c670a06c3c
basma2002 <basma.mohammed1@ocsbstudent.ca>
2019-10-31 10:18:53 -0400

    update

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float forwardInput;
+    public float backInput;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        forwardInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.back * Time.deltaTime * speed * backInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+
+        // move the place forward 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // private variables
+    private float speed = 5.0f;
+    private float turnspeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // we turn the vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput);
+       
+    }
+}
